029415972812bed703cb276de8f253b1d625b9f2
Roy Fleur <alyamarican@student.usm.my>
2024-01-12 21:18:58 +0800

    [update] whereRaw code added but not used yet

--- (2)/eLegal/resources/views/dbconn.blade.php
+++ (2)/eLegal/resources/views/dbconn.blade.php
@@ -13,15 +13,34 @@
                 $conn = new PDO("mysql:host=localhost;dbname=dummy","root","");
                 
                 // to catch & hanlde db-related exceptions
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                // to fetch query
-                $query = "SELECT * FROM cases";
-                $result = $conn->query($query);
-                
-                // to print fetched data
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);             
+                                
                 echo "connected successfully to database ".$conn->query("SELECT DATABASE()")->fetchColumn() . PHP_EOL;
                 echo "<br>";
+
+                /* // fetch data using eloquent query
+                $id = 1; // replace with the actual value of the id
+                $post = DB::table('cases')
+                        ->where('caseID', 'caseDesc', 'caseStatus', 'assignedTo')
+                        ->whereRaw('caseID ='.$id)
+                        ->first();
+                        
+                // to print fetched data
+                if($cases){
+                    echo "Fetched data from 'cases' table: ". PHP_EOL;
+                    echo "caseId: " . $cases->caseID . PHP_EOL;
+                    echo "caseDesc: " . $cases->caseDesc . PHP_EOL;
+                    echo "caseStatus: " . $cases->caseStatus . PHP_EOL;
+                    echo "assignedTo: " . $cases->assignedTo . PHP_EOL;
+                }
+                else{
+                    echo "no data found";
+                } */
+
+                // to fetch query & print w/o eloquent
+                $query = "SELECT * FROM cases";
+                $result = $conn->query($query);
+
                 foreach($result as $row){
                     echo $row['caseID']." ".$row['caseDesc']." ".$row['caseStatus']." ".$row['assignedTo']."<br>";
                 }
